91e518611b53117a0b25dcfc8445873aa5c0c56c
AnitaMevada <anita.mevada23717@paruluniversity.ac.in>
2024-04-03 10:19:35 +0530

    Updated djangoproj/settings.py file

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['localhost',
+                 "https://akinbamigben-8000."
+                 "theianext-0-labs-prod-misc-tools-us-east-0.proxy"
+                 ".cognitiveclass.ai/"]
+CSRF_TRUSTED_ORIGINS = ['https://akinbamigben-8000.'
+                        'theianext-0-labs-prod-misc-tools-us-east-0.proxy'
+                        '.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,7 +66,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,7 +99,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -134,5 +144,8 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
-
+STATICFILES_DIRS = [
+                        os.path.join(BASE_DIR, 'frontend/static'),
+                        os.path.join(BASE_DIR, 'frontend/build'),
+                        os.path.join(BASE_DIR, 'frontend/build/static'),
+                    ]
