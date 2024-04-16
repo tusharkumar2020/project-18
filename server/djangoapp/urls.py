6f621b715c0aabfd184e657b827645af6847becc
anajima13 <a840082303@my.uvm.edu.mx>
2024-04-16 11:40:39 -0600

    Adding temporary changes to  Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,97 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+# Uncomment the required imports before adding the code
 
-app_name = 'djangoapp'
-urlpatterns = [
-    # # path for registration
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
+import logging
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .populate import initiate
 
-    # path for dealer reviews view
 
-    # path for add a review view
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Create your views here.
+
+# Create a `login_request` view to handle sign in request
+@csrf_exempt
+def login_user(request):
+    # Get username and password from request.POST dictionary
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    # Try to check if provide credential can be authenticated
+    user = authenticate(username=username, password=password)
+    data = {"userName": username}
+    if user is not None:
+        # If user is valid, call login method to login current user
+        login(request, user)
+        data = {"userName": username, "status": "Authenticated"}
+    return JsonResponse(data)
+
+# Create a `logout_request` view to handle sign out request
+def logout_request(request):
+    logout(request)
+    data = {"userName": "", "status": "Logged out"}
+    return JsonResponse(data)
+# ...
+
+# Create a `registration` view to handle sign up request
+@csrf_exempt
+def registration(request):
+    context = {}
+    
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        # check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        #If not, simply log this is a new user
+        logger.debug("{} is a new user".format(username))
+    
+    #If it is a new user, create a new user
+    if not username_exist:
+        #Create user in auth_user table
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+        #Login user and redirect to list page
+        login(request, user)
+        data = {"userName":username,"status": "Authenticated"}
+        return JsonResponse(data)
+    else : 
+        data = {"userName":username, "error":"Already exists"}
+        return JsonResponse(data)
+# ...
+
+# # Update the `get_dealerships` view to render the index page with
+# a list of dealerships
+# def get_dealerships(request):
+# ...
+
+# Create a `get_dealer_reviews` view to render the reviews of a dealer
+# def get_dealer_reviews(request,dealer_id):
+# ...
+
+# Create a `get_dealer_details` view to render the dealer details
+# def get_dealer_details(request, dealer_id):
+# ...
+
+# Create a `add_review` view to submit a review
+# def add_review(request):
+# ...
