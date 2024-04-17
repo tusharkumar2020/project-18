61a2cd9d598869024cbda119caadad54e5f45acd
anajima13 <a840082303@my.uvm.edu.mx>
2024-04-17 13:19:24 -0600

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,97 +1,59 @@
-# Uncomment the required imports before adding the code
+# Uncomment the imports before you add the code
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+app_name = 'djangoapp'
+urlpatterns = [
+    # path for registration
+    path(
+        route='register',
+        view=views.registration,
+        name='registration'
+    ),
+    # path for login
+    path(
+        route='login',
+        view=views.login_user,
+        name='login'
+    ),
+    path(
+        route='logout',
+        view=views.logout_request,
+        name='logout'
+    ),
+    # path for dealer reviews view
+    path(
+        route='get_cars',
+        view=views.get_cars,
+        name='getcars'
+    ),
+    path(
+        route='get_dealers',
+        view=views.get_dealerships,
+        name='get_dealers'
+    ),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    # path for add a review view
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_details'
+    ),
+    path(
+        route='add_review',
+        view=views.add_review,
+        name='add_review'
+    ),
 
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
-import logging
-import json
-from django.views.decorators.csrf import csrf_exempt
-from .populate import initiate
-
-
-# Get an instance of a logger
-logger = logging.getLogger(__name__)
-
-
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
-@csrf_exempt
-def login_user(request):
-    # Get username and password from request.POST dictionary
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    # Try to check if provide credential can be authenticated
-    user = authenticate(username=username, password=password)
-    data = {"userName": username}
-    if user is not None:
-        # If user is valid, call login method to login current user
-        login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
-    return JsonResponse(data)
-
-# Create a `logout_request` view to handle sign out request
-def logout_request(request):
-    logout(request)
-    data = {"userName": "", "status": "Logged out"}
-    return JsonResponse(data)
-# ...
-
-# Create a `registration` view to handle sign up request
-@csrf_exempt
-def registration(request):
-    context = {}
-    
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    username_exist = False
-    email_exist = False
-    try:
-        # check if user already exists
-        User.objects.get(username=username)
-        username_exist = True
-    except:
-        #If not, simply log this is a new user
-        logger.debug("{} is a new user".format(username))
-    
-    #If it is a new user, create a new user
-    if not username_exist:
-        #Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
-        #Login user and redirect to list page
-        login(request, user)
-        data = {"userName":username,"status": "Authenticated"}
-        return JsonResponse(data)
-    else : 
-        data = {"userName":username, "error":"Already exists"}
-        return JsonResponse(data)
-# ...
-
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
-
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
-
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]'] 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'https://missspidey-8080.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] 
 CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
