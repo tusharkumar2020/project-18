c32981b3cecb083e18a76a519af84ec4441c2519
ronennaz3 <ronennaz3@gmail.com>
2024-04-22 06:30:24 -0400

    get cars

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://ronennaz3-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://ronennaz3-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'https://ronennaz3-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 'https://ronennaz3-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://ronennaz3-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 'https://ronennaz3-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -88,7 +88,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'PASSWORD': 'MzE5ODEtcm9uZW5u',
     }
 }
 
