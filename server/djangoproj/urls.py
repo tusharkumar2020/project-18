1b614457b2892067152b78401ba863dd0a3c3933
olgarpsb <olgarpsb@gmail.com>
2024-04-23 09:45:20 -0500

    transfer to github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,6 +22,8 @@ from django.conf import settings
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
