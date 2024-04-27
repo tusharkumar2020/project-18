51d8342355268cf73de2c813d39c147bff9976bf
ronennaz3 <157146858+ronennaz3@users.noreply.github.com>
2024-04-27 23:24:22 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -10,7 +10,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -39,10 +39,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-            validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    
+                validators=[
+                    MaxValueValidator(2023),
+                    MinValueValidator(2015)
+                    ])
+
     def __str__(self):
         return self.name  # Return the name as the string representation
