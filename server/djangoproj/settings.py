584c0688523d8b7a005e2fa4b84ac6d7ee198a78
ronennaz3 <157146858+ronennaz3@users.noreply.github.com>
2024-04-28 01:25:03 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-from django.contrib.auth.password_validation
+from django.contrib.auth.password_validation:
     import UserAttributeSimilarityValidator
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
