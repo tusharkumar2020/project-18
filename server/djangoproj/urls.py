a74f9cc9583befdb4cd8c8da36865cdc910bbdde
anajima13 <a840082303@my.uvm.edu.mx>
2024-04-29 13:23:17 -0600

    changes to vscode

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
+    # All other paths
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
