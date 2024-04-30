b7681a2f6107cf1a06721685dd2b6b26dff9ca07
anajima13 <a840082303@my.uvm.edu.mx>
2024-04-30 09:38:54 -0600

    changes 2 models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class CarMake(models.Model): 
-    name = models.CharField (max_length=100) 
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -45,4 +38,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-    
