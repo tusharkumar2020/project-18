b66feb7916667884af16dc8837acdd1807df2a54
anajima13 <a840082303@my.uvm.edu.mx>
2024-04-30 10:33:00 -0600

    changes to appjs

--- server/database/app.js
+++ server/database/app.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
 app.use(cors())
@@ -30,7 +30,6 @@ try {
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
 }
-
 
 // Express route to home
 app.get('/', async (req, res) => {
@@ -113,19 +112,19 @@ try {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  let new_id = documents[0].id + 1;
 
   const review = new Reviews({
-        "id": new_id,
-        "name": data['name'],
-        "dealership": data['dealership'],
-        "review": data['review'],
-        "purchase": data['purchase'],
-        "purchase_date": data['purchase_date'],
-        "car_make": data['car_make'],
-        "car_model": data['car_model'],
-        "car_year": data['car_year'],
-    });
+    id: new_id,
+    name: data.name,
+    dealership: data.dealership,
+    review: data.review,
+    purchase: data.purchase,
+    purchase_date: data.purchase_date,
+    car_make: data.car_make,
+    car_model: data.car_model,
+    car_year: data.car_year
+});
 
   try {
     const savedReview = await review.save();
