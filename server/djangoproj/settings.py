e2863e92cd5619305c910b979e7c8bf0d9f0da81
anajima13 <a840082303@my.uvm.edu.mx>
2024-04-30 10:50:39 -0600

    chages

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -17,7 +17,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = os.getenv(
+    'DJANGO_SECRET_KEY', 
+    default='django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
