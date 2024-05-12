0d150192e8fc3c48688e214005457b2c78c42fb0
sandroalfonso <sandrozzzy30@gmail.com>
2024-05-12 06:20:24 -0400

    2nd commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,9 @@ urlpatterns = [
     # path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
-
+path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+path(route='add_review', view=views.add_review, name='add_review'),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,7 +7,7 @@
 # from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
-
+from .restapis import get_request, analyze_review_sentiments, post_review
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -49,8 +49,14 @@ def login_user(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
-# ...
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
