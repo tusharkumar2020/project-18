26f1d471960d96bc55c01161f1ef77c5f6d35d78
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-14 19:32:26 +0800

    updated urls

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,6 +16,10 @@ urlpatterns = [
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='add_review', view=views.add_review, name='add_review'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+
+    # 添加获取经销商评论的路径
+    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
 
     # path for dealer reviews view
 
