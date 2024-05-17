11d6fe6f79e51db5bf478ab4ca3acdc3c1327e86
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-17 17:37:52 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -89,10 +89,12 @@ def registration(request):
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 def get_dealerships(request):
+    state = "All"
+    
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
+        endpoint = "/fetchDealers/" + state
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
