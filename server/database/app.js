371ee3776dfe94ed132f298f319b2cfd984ab56d
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-17 15:58:02 -0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -52,23 +52,42 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
     const documents = await Reviews.find({dealership: req.params.id});
     res.json(documents);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
+    res.status(500).json({ error: 'Error fetching Reviews for dealship documents' });
   }
 });
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const documents = await Dealers.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching all dealerships documents' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const documents = await Reviews.find({dealership: req.params.state});
+        res.json(documents);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealships by state documents' });
+      }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const documents = await Reviews.find({dealership: req.params.id});
+        res.json(documents);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealships by Id documents' });
+      }
+
 });
 
 //Express route to insert review
@@ -102,3 +121,4 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
