b4a8ea4bffabdd47b8834e01a5fd0399ed669c29
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-18 13:40:13 -0500

    Update Dealers.jsx

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -33,17 +33,79 @@ const Dealers = () => {
     const retobj = await res.json();
     if(retobj.status === 200) {
       //Array.from(retobj.dealers)
-      let all_dealers = [{"id": 5,
-                          "city": "Baltimore",
-                          "state": "Maryland",
-                          "st": "MD",
-                          "address": "93 Golf Course Pass",
-                          "zip": "21203",
-                          "lat": 39.2847,
-                          "long": -76.6205,
-                          "short_name": "Regrant",
-                          "full_name": "Regrant Car Dealership"
-                        }];
+      let all_dealers = [{
+                            "id": 1,
+                            "city": "El Paso",
+                            "state": "Texas",
+                            "st": "TX",
+                            "address": "3 Nova Court",
+                            "zip": "88563",
+                            "lat": 31.6948,
+                            "long": -106.3,
+                            "short_name": "Holdlamis",
+                            "full_name": "Holdlamis Car Dealership"
+                          },
+                          {
+                            "id": 2,
+                            "city": "Minneapolis",
+                            "state": "Minnesota",
+                            "st": "MN",
+                            "address": "6337 Butternut Crossing",
+                            "zip": "55402",
+                            "lat": 44.9762,
+                            "long": -93.2759,
+                            "short_name": "Temp",
+                            "full_name": "Temp Car Dealership"
+                          },
+                          {
+                            "id": 3,
+                            "city": "Birmingham",
+                            "state": "Alabama",
+                            "st": "AL",
+                            "address": "9477 Twin Pines Center",
+                            "zip": "35285",
+                            "lat": 33.5446,
+                            "long": -86.9292,
+                            "short_name": "Sub-Ex",
+                            "full_name": "Sub-Ex Car Dealership"
+                          },
+                          {
+                            "id": 4,
+                            "city": "Dallas",
+                            "state": "Texas",
+                            "st": "TX",
+                            "address": "85800 Hazelcrest Circle",
+                            "zip": "75241",
+                            "lat": 32.6722,
+                            "long": -96.7774,
+                            "short_name": "Solarbreeze",
+                            "full_name": "Solarbreeze Car Dealership"
+                          },
+                          {
+                            "id": 5,
+                            "city": "Baltimore",
+                            "state": "Maryland",
+                            "st": "MD",
+                            "address": "93 Golf Course Pass",
+                            "zip": "21203",
+                            "lat": 39.2847,
+                            "long": -76.6205,
+                            "short_name": "Regrant",
+                            "full_name": "Regrant Car Dealership"
+                          },
+                          {
+                            "id": 6,
+                            "city": "Wilkes Barre",
+                            "state": "Pennsylvania",
+                            "st": "PA",
+                            "address": "2 Burrows Hill",
+                            "zip": "18763",
+                            "lat": 41.2722,
+                            "long": -75.8801,
+                            "short_name": "Stronghold",
+                            "full_name": "Stronghold Car Dealership"
+                          }]
+                        
       let states = [];
       all_dealers.forEach((dealer)=>{
         states.push(dealer.state)
