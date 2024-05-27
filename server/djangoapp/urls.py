c7eb1c4e2dc427d8b54c48b8f9a3fdabf950e489
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-27 21:51:29 +0800

    Fix lint errors in urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,12 +14,24 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
     path(route='add_review', view=views.add_review, name='add_review'),
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path(
+        'dealer/<int:dealer_id>/',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
 
     # 添加获取经销商评论的路径
-    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
+    path(
+        'reviews/dealer/<int:dealer_id>/',
+        views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
 
     # path for dealer reviews view
 
