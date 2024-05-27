381992a94b50db7c2239baa92f9caa5d903bc2ed
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-27 21:01:47 +0800

    Revert "lint test"
    This reverts commit baad13fd6acf4eb7985c128f1293fd5e4ba43f29.

--- server/database/inventory.js
+++ server/database/inventory.js
@@ -1,29 +1,30 @@
+const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const cars = new Schema({
-  dealer_id: {
+dealer_id: {
+    type: Number,
+    required: true
+},
+make: {
+    type: String,
+    required: true
+  },
+model: {
+    type: String,
+    required: true
+  },
+bodyType: {
+    type: String,
+    required: true
+  },
+year: {
     type: Number,
     required: true
   },
-  make: {
-    type: String,
-    required: true
-  },
-  model: {
-    type: String,
-    required: true
-  },
-  bodyType: {
-    type: String,
-    required: true
-  },
-  year: {
-    type: Number,
-    required: true
-  },
-  mileage: {
+mileage: {
     type: Number,
     required: true
   }
