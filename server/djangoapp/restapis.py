62a254781ac56585815c1204b92ccaec9777ba8f
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-27 21:49:11 +0800

    Fix lint errors in restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,19 +3,23 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default="http://localhost:3030"
+)
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5050/"
+)
+
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if kwargs:  # <--- 修改行
-        for key, value in kwargs.items():  # <--- 修改行
-            params = params + key + "=" + value + "&"  # <--- 修改行
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
@@ -29,6 +33,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print(f"Network exception occurred: {e}")
         return None
+
 
 # Add code for get requests to back end
 
@@ -44,19 +49,19 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # Add code for retrieving sentiments
 
 def post_review(data_dict):
 
     request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url, json=data_dict)  # data_dict 作为载荷也就是需要推送的数据
+        response = requests.post(
+            request_url, json=data_dict
+        )  # data_dict 作为载荷也就是需要推送的数据
         response.raise_for_status()  # 检查请求是否成功
         response_data = response.json()
         print(response_data)
         return response_data
-    except requests.exceptions.RequestException as e:  # <--- 修改行
-        print(f"Network exception occurred: {e}")  # <--- 修改行
-
-# Add code for posting review
-
+    except requests.exceptions.RequestException as e:
+        print(f"Network exception occurred: {e}")
