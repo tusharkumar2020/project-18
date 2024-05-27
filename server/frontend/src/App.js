77af33227de9738b4297a244d024b5999721080e
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-27 21:00:37 +0800

    Revert "simple test 1"
    This reverts commit a7da17671790ca6983501e1897d109d0f472a388.

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import Register from "./components/Register/Register";
+import LoginPanel from "./components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Dealers from "./components/Dealers/Dealers";
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Routes>
+      <Route 
+        path="/" 
+        element={<iframe src="/static/Home.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} 
+      />
+      <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+      <Route 
+        path="/about" 
+        element={<iframe src="/static/About.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} 
+      />
+      <Route 
+        path="/contact" 
+        element={<iframe src="/static/Contact.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} 
+      />
+    </Routes>
   );
 }
 
