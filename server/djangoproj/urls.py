a418af178bc067d6f49f361d1bd146f6afe8579b
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-27 22:01:11 +0800

    Fix lint errors in urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -7,8 +7,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -18,20 +17,46 @@ from django.urls import path, include
 from django.views.generic import TemplateView, RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
-from djangoapp.views import logout_request  # 确保从djangoapp应用导入logout_request视图
+from djangoapp.views import logout_request
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('logout/', logout_request, name='logout'),  # 直接添加注销路径
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-    path('manifest.json', RedirectView.as_view(url='/static/manifest.json', permanent=True)),
+    path(
+        'about/', 
+        TemplateView.as_view(template_name="About.html")
+    ),
+    path(
+        'contact/', 
+        TemplateView.as_view(template_name="Contact.html")
+    ),
+    path(
+        'register/', 
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'login/', 
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path('logout/', logout_request, name='logout'),
+    path(
+        '', 
+        TemplateView.as_view(template_name="Home.html")
+    ),
+    path(
+        'dealers/', 
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'dealer/<int:dealer_id>/', 
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<int:dealer_id>', 
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'manifest.json', 
+        RedirectView.as_view(url='/static/manifest.json', permanent=True)
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
