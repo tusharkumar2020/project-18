a7da17671790ca6983501e1897d109d0f472a388
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-27 17:40:30 +0800

    simple test 1

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,31 +1,10 @@
-import Register from "./components/Register/Register";
-import LoginPanel from "./components/Login/Login";
-import { Routes, Route } from "react-router-dom";
-import Dealers from "./components/Dealers/Dealers";
-import Dealer from "./components/Dealers/Dealer";
-import PostReview from "./components/Dealers/PostReview";
+import React from 'react';
 
 function App() {
   return (
-    <Routes>
-      <Route 
-        path="/" 
-        element={<iframe src="/static/Home.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} 
-      />
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers />} />
-      <Route path="/dealer/:id" element={<Dealer />} />
-      <Route path="/postreview/:id" element={<PostReview />} />
-      <Route 
-        path="/about" 
-        element={<iframe src="/static/About.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} 
-      />
-      <Route 
-        path="/contact" 
-        element={<iframe src="/static/Contact.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} 
-      />
-    </Routes>
+    <div>
+      Hello World
+    </div>
   );
 }
 
