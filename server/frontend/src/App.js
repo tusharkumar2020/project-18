8cf9329f5a0055d7b8464556b1264f8984ada6fa
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-05-31 18:22:32 +0800

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,18 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<iframe src="/static/Home.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers />} />
       <Route path="/dealer/:id" element={<Dealer />} />
       <Route path="/postreview/:id" element={<PostReview />} />
-      <Route path="/about" element={<iframe src="/static/About.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} />
-      <Route path="/contact" element={<iframe src="/static/Contact.html" style={{ width: '100%', height: '100vh', border: 'none' }} />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
