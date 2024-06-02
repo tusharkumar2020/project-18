25ea3c4f1020ee1e1615ca8549efae5d6ef810c5
sairao1234 <131871849+sairao1234@users.noreply.github.com>
2024-06-02 16:34:36 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,6 +22,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+        path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+        path('dealers/', TemplateView.as_view(template_name="index.html")),
         path('register/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
         path('about/', TemplateView.as_view(template_name="About.html")),
