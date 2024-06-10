249190746a08c16e90f0278cfd8d4ea32cdef73e
mekebib <mekebibteka@yahoo.com>
2024-06-10 00:11:36 -0400

    https://mekebibteka-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,29 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
+from django.urls import path, re_path
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.conf import settings
-# from . import views
+from django.conf.urls.static import static
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    
+    path(route='register', view=views.register, name='register'),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+
+   
+    path(route='login', view=views.login_user, name='login'),
+
+    # Serve favicon.ico
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=True)),
+
+    # Serve manifest.json
+    re_path(r'^manifest\.json$', RedirectView.as_view(url=staticfiles_storage.url('manifest.json'), permanent=True)),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
     # path for dealer reviews view
 
     # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
