43cc23d6fe085ebcc9831cf84aa7db39ba7ce00a
tptam <roseisroseisrose@gmail.com>
2024-06-18 13:26:54 -0400

    Complete Module 3: Create Django Proxy Services of Backend APIs

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,6 +18,7 @@ from .models import CarMake, CarModel
 
 from .populate import initiate
 
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -100,17 +101,46 @@ def get_cars(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
-# ...
+def get_dealerships(request, state="ALL"):
+    if (state == "ALL"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/" + state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request,dealer_id):
+    if (dealer_id) :
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            print(review_detail['review'])
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            if response is not None:
+                review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if (dealer_id) :
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealer = get_request(endpoint)
+        return JsonResponse({"status": 200, "dealer": dealer})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+def add_review(request):
+    if (request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
