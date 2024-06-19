ed51e581c7131aae5883e2719204f17aa311909e
isaachuangx <36714827+isaachuangx@users.noreply.github.com>
2024-06-19 21:55:24 +0800

    Update SearchCars.jsx

--- server/frontend/src/components/Dealers/SearchCars.jsx
+++ server/frontend/src/components/Dealers/SearchCars.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import "./Dealers.css";
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Header from '../Header/Header';
 
 const SearchCars = () => {
