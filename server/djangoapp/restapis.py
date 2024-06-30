18e247a38c854d87383c93f1da66d420b0ac7adb
lily <harvey.lily98@gmail.com>
2024-06-30 13:40:04 -0400

    added proxy

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -31,5 +31,11 @@ def get_request(endpoint, **kwargs):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 
-# def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
