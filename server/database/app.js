d081c7c08baf5dd9b50129e4d23a7601b5cbcd90
Bifenaa <fbergerondev@gmail.com>
2024-07-01 19:11:05 -0400

    Updated finale project

--- server/database/app.js
+++ server/database/app.js
@@ -47,29 +47,45 @@ app.get('/fetchReviews', async (req, res) => {
 });
 
 // Express route to fetch reviews by a particular dealer
-app.get('/fetchReviews/dealer/:id', async (req, res) => {
-  try {
-    const documents = await Reviews.find({dealership: req.params.id});
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
-  }
-});
-
-// Express route to fetch all dealerships
-app.get('/fetchDealers', async (req, res) => {
-//Write your code here
-});
+app.get("/fetchReviews/dealer/:id", async (req, res) => {
+    try {
+      const reviewsByDealer = await Reviews.find({ dealership: req.params.id });
+      res.json(reviewsByDealer);
+    } catch (error) {
+      res.status(500).json({ error: "Error fetching documents" });
+    }
+  });
+  
+  // Express route to fetch all dealerships
+  app.get("/fetchDealers", async (req, res) => {
+    //Write your code here
+    try {
+      const dealers = await Dealerships.find();
+      res.json(dealers);
+    } catch (error) {
+      res.status(500).json({ error: "Error fetching documents" });
+    }
+  });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
-});
-
-// Express route to fetch dealer by a particular id
-app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
-});
+    try {
+        const dealers = await Dealerships.find({ state: req.params.state });
+        res.json(dealers);
+    } catch (err) {
+         res.status(500).json({ error: 'Error fetching documents' });
+     }
+    });
+    
+    // Express route to fetch dealer by a particular id
+    app.get('/fetchDealer/:id', async (req, res) => {
+        try {
+            const documents = await Dealerships.find({id: req.params.id });
+            res.json(documents);
+          } catch (err) {
+            res.status(500).json({ error: 'Error fetching documents' });
+          }
+    });
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
