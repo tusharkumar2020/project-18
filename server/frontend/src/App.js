cc0f1cc496c0b67e1eed9656b6fd8735afeb9a86
Bifenaa <fbergerondev@gmail.com>
2024-07-01 18:41:05 -0400

    changes made to the final project

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,16 @@
 import LoginPanel from "./components/Login/Login"
+import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
+
+
+// Register route code
+//<Route path="/register" element={<Register />} /> from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+        <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
