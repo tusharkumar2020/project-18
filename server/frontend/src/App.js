0870de1d1b23fce384fbdeb316297eb09f430e81
Bifenaa <fbergerondev@gmail.com>
2024-07-04 17:37:11 -0400

    Changes for review

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,8 +2,8 @@ import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
-import PostReview from "./components/Dealers/PostReview"
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview";
 
 // Register route code
 //<Route path="/register" element={<Register />} /> from "./components/Register/Register";
