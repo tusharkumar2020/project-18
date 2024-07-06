30530ffdb4b17c4711c592042738cfb4ed6b3f63
microdreamit <shahanur700@gmail.com>
2024-07-06 01:33:36 -0400

    hello

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,9 @@ from django.conf import settings
 from djangoapp import views
 
 urlpatterns = [
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path(route='login', view=views.login_user, name='login'),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
