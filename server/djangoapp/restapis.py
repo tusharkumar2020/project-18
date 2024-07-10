f8acb22626a3abcc1e7680289779000f231757bb
Bifenaa <fbergerdev@gmail.com>
2024-07-10 15:05:28 -0400

    Flint Pushes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,7 +23,7 @@ def get_request(endpoint, **kwargs):
 
     request_url = backend_url+endpoint+"?"+params
 
-    print ("GET from {} ".format(request_url))
+    print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
