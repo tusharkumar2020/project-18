88ff09361e8fe9faeb4b5f2b1d6bbe836812f234
Bifenaa <fbergerondev@gmail.com>
2024-07-12 10:34:22 -0400

    chagnes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost', 
     'https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-                 
+             
 CSRF_TRUSTED_ORIGINS = [
     'https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
@@ -98,8 +98,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
 
-     'NAME':
-     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    'NAME':
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
 
     },
     {
