758546d91b5e83870cf4f0c48dfeaddab997356a
Bifenaa <fbergerondev@gmail.com>
2024-07-12 10:48:00 -0400

    Address CHanges

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-   
+    'https://' +
+    'fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.' +
+    'proxy.cognitiveclass.ai']
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://' +
+    'fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01' +
+    '.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
