9f1e6261c1aa7bc82f7617f7ac9679b78fac3b7e
Bifenaa <fbergerondev@gmail.com>
2024-07-12 09:14:23 -0400

    additional changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = 
+['localhost','https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+
+CSRF_TRUSTED_ORIGINS = 
+['https://fabienberger-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,24 +96,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-
+    'NAME':
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
-
+    'NAME':
+    'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+    'NAME':
+    'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+    'NAME':
+    'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
