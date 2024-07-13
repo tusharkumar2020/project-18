b8ed2dee5a02fde9650aea82710a22288e026089
“Charles <Charlesobika@yahoo.com>
2024-07-13 11:05:19 -0400

    init nodecar message

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,16 +28,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< Updated upstream
+
 ALLOWED_HOSTS = ['localhost','https://chijiokeobik-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 CSRF_TRUSTED_ORIGINS = ['https://chijiokeobik-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-=======
+
 ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = [
-    'https://chijiokeobik-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://chijiokeobik-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 
 ]
->>>>>>> Stashed changes
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -70,15 +69,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-<<<<<<< Updated upstream
-            os.path.join(BASE_DIR,'frontend/static')
-=======
+        'DIRS': [ 
             os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
-      
->>>>>>> Stashed changes
+       
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -152,14 +147,10 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-<<<<<<< Updated upstream
-     os.path.join(BASE_DIR,'frontend/static')
-=======
+STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 
->>>>>>> Stashed changes
 ]
 
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,11 +23,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
-<<<<<<< Updated upstream
+
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
-=======
+
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
->>>>>>> Stashed changes
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
