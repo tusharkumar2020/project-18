8cfec40180cee654abbb93e55c4a74737b9e7f45
Bifenaa <fbergerondev@gmail.com>
2024-07-15 18:35:52 -0400

    Updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -21,6 +21,7 @@ class CarMake (models.Model):
 
 
 class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
