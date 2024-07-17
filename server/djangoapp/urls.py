8ab4892d63d08cb1b9a6526d5528ceb4bac168e7
Pranay Raj <164135248+PranayRaj1007@users.noreply.github.com>
2024-07-18 01:38:18 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='registration', view=views.registration,name='registration'),
     path(route='register', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
