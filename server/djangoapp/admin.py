877bf8f8cd4947283791692cde36aaa875cd1dd9
ELCALRUZO <Charlesobika@yahoo.com>
2024-07-24 17:21:11 +0100

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from .models import CarMake, CarModel, Dealership, Review
-
- 
+# Fix blank line containing whitespace
+# Ensure this file follows the PEP 8 standard for blank lines and other common issues
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)
 admin.site.register(Dealership)
 admin.site.register(Review)
 # CarModelInline class
-
 # CarModelAdmin class
-
 # CarMakeAdmin class with CarModelInline
-
 # Register models here
