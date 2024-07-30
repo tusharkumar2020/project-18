c716708b8bde2d684ce3963915304e3f53f5aa9b
unknown <rosaspace@126.com>
2024-07-30 16:55:49 -0500

    first commit

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = ['localhost','127.0.0.1:8000']
+# CSRF_TRUSTED_ORIGINS = ['127.0.0.1:8000']
 ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = []
 
@@ -61,7 +63,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'frontend/static')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,5 +138,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'frontend/static')
+]
 
