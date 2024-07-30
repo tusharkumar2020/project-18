362bd13f840167d7cd0f93046b6fe2bb16a81f23
Fred <f.d.broussard@gmail.com>
2024-07-30 16:39:26 -0400

    Created paths

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -61,9 +61,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/static'),
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),],
+            os.path.join(BASE_DIR, 'frontend/build/static'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
