5bea4fa34d53431ced2fbc1bab0247d2bd28a63d
gregdorian <greg_dorian@yahoo.com>
2024-07-31 17:46:35 -0400

    modulo4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,5 @@
 import LoginPanel from "./components/Login/Login"
+import Register from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
 
@@ -6,8 +7,10 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
-      
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
