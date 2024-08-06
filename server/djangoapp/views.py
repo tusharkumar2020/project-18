e97d002c93d675c9c6c5ab9dc524905b9544e55c
rockbjson <tanisidas.21@gmail.com>
2024-08-06 14:23:24 +0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -115,9 +115,9 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 def add_review(request):
     if (request.user.is_anonymous is False):
-        #data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
-            #response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse(
