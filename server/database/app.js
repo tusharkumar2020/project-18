0d6fc38b2c4e1eb557f0734fcfe9272a979bf3c0
rosaspace <rosaspace@126.com>
2024-08-05 20:37:51 -0500

    format js files

--- server/database/app.js
+++ server/database/app.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -93,18 +95,18 @@ try {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
   const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  let new_id = documents[0].id + 1
 
   const review = new Reviews({
 		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
+		"name": data.name,
+		"dealership": data.dealership,
+		"review": data.review,
+		"purchase": data.purchase,
+		"purchase_date": data.purchase_date,
+		"car_make": data.car_make,
+		"car_model": datacar_model,
+		"car_year": data.car_year,
 	});
 
   try {
