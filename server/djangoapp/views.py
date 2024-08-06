6921e7282ebe264155895c87bf35583fe65bed7b
rosaspace <rosaspace@126.com>
2024-08-05 21:10:57 -0500

    format python files

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -130,7 +130,7 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 # def add_review(request):
-def add_review(request):    
+def add_review(request):
     if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
