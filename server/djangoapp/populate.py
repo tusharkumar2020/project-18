ee96ae7d0ff8d1283611ba0eae21d266e25e73c5
abondaruk <95693431+abondaruk@users.noreply.github.com>
2024-08-08 15:57:59 +0300

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -50,7 +50,7 @@ def initiate():
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(
+        CarModel.objects.create(
                     name=data['name'],
                     car_make=data['car_make'],
                     type=data['type'],
