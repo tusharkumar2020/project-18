c59dc79fb6ebd7ade1ae18a4d053c74a85bc8409
abondaruk <95693431+abondaruk@users.noreply.github.com>
2024-08-08 16:59:12 +0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -46,7 +46,7 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = request.post(request_url, json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except Exception:
