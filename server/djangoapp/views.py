343f0644ee83c4be6c098346afd7153bfe5d7538
abondaruk <95693431+abondaruk@users.noreply.github.com>
2024-08-08 16:57:38 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -95,7 +95,7 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
+        cars.append({"CarModel": car_model.name,
                      "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
