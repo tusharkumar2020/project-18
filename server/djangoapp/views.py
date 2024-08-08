6f4e86d09c78ef50d5ad6895a06e60f4f60ba191
abondaruk <95693431+abondaruk@users.noreply.github.com>
2024-08-08 17:40:09 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,11 +1,11 @@
 # Uncomment the required imports before adding the code
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
+# from django.contrib import messages
 from datetime import datetime
 
 from django.http import JsonResponse
