69bd2c83bcba2cb29746875c2d0aeb97823979d5
gregdorian <gregdorian>
2024-08-08 12:45:17 -0400

    una linea en models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -38,7 +38,10 @@ class CarModel(models.Model):
     engine = models.CharField(max_length=100, blank=True, null=True)
     fuel_type = models.CharField(max_length=50, blank=True, null=True)
     transmission = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(
+    max_digits=10, decimal_places=2, blank=True, null=True
+    )
+
 
     def __str__(self):
         return f"{self.name} ({self.car_make.name})"
