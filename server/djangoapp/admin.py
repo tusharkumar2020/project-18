ff6f3ef91bba4e9a6cf894a2ac95fb0bca31ed27
jjchiramal <jjchiramal@gmail.com>
2024-08-13 13:00:43 -0400

    Module 2 - Updated User Management Modules

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,17 +1,13 @@
-from django.contrib import admin
-from .models import CarMake, CarModel
-
-# Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
-
-
+# from django.contrib import admin
+# from .models import related models
+ 
+ 
 # Register your models here.
-
+ 
 # CarModelInline class
-
+ 
 # CarModelAdmin class
-
+ 
 # CarMakeAdmin class with CarModelInline
-
+ 
 # Register models here
