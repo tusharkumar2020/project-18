e5c7f9a3881b222928a3580973662c2fb198e821
gregdorian <greg_dorian@yahoo.com>
2024-08-15 12:15:34 -0400

    ajustes para lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -98,6 +98,7 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
+
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
 
@@ -110,19 +111,19 @@ def get_dealer_reviews(request, dealer_id):
             return JsonResponse({"status": 500, "message": "Error on reviews"})
 
         for review_detail in reviews:
-            if (
-                'review' in review_detail and
-                review_detail['review'] is not None
-                ):
+            if ('review' in review_detail and
+                review_detail['review'] is not None):
                 sentiment = analyze_review_sentiments(review_detail['review'])
                 print(sentiment)
-                review_detail['sentiment'] = sentiment.get('sentiment', 'unknown')
+                review_detail['sentiment'] = sentiment.get('sentiment', 
+                'unknown')
             else:
                 review_detail['sentiment'] = 'unknown'
 
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
@@ -132,6 +133,7 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status": 200, "dealer": dealership})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 # Create a `add_review` view to submit a review
 def add_review(request):
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,14 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://germa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://germa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'https://germa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://germa-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01'
+    '.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
