709853a95c1ad966dcb2a83582d2e8df0ad1df07
rom-code <raad.cars@gmail.com>
2024-09-08 18:28:47 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,8 +1,8 @@
+
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
-# Register your models here.
+# Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)
 
