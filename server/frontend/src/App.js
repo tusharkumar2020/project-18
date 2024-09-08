5e6b350061d4ee8d7992f2f561f7110ca579f636
rom-code <raad.cars@gmail.com>
2024-09-08 06:42:47 -0400

    register, login, logout function

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import RegistrationPanel from './components/Register/Register';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
-  );
-}
+    return (
+        <Routes>
+          {/* Define routes for different components */}
+          
+          <Route path="/login" element={<LoginPanel />} />
+          <Route path="/register" element={<RegistrationPanel />} />
+          
+    
+        </Routes>
+      );
+    }
 export default App;
