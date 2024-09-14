8a526eab173a8d78724b7b5ab25cb6f3ec924786
Molka Tayahi <Tayahi.molka@gmail.com>
2024-09-14 20:58:45 +0200

    Add kbcl

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -27,8 +27,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
@@ -41,10 +43,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
+
     # Other fields as needed
 
     def __str__(self):
