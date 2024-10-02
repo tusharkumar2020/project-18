cc90ffaeb502d78dfff0c2786d16fdc56d5316bd
darisha666 <mikhailovskayadarina@gmail.com>
2024-10-02 09:26:01 -0400

    Описание изменений

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,7 +19,10 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = [    
+    path('register/', TemplateView.as_view(template_name="index.html")),   
+    path('login/', TemplateView.as_view(template_name="index.html")),      
+    path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
