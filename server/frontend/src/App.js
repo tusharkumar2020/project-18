8a528e83de2fd2c44e516ab7aae351620705af7f
poduro-code <oduro.prince383@gmail.com>
2024-10-10 15:40:45 -0400

    Upadted logout and also register

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,4 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 import React from 'react';
@@ -7,9 +7,9 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />  {/* Register route */}
     </Routes>
-
   );
 }
-export default App;
 
+export default App;
