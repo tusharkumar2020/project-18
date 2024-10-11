cd6cc4508724fe0fc053dc05798ffc08f5fb5b5b
poduro-code <oduro.prince383@gmail.com>
2024-10-11 18:48:00 -0400

    updaed car models and also admin page

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oduroprince3-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
-    'https://oduroprince3-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://oduroprince3-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
