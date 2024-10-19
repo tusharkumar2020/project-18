a9a4844cc9891d4670be5fac69cda69676cf0745
Amreyes2 <amreyes.2@gmail.com>
2024-10-19 13:27:10 -0600

    added about and contact

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['http://localhost']
 
 REST_FRAMEWORK = {
