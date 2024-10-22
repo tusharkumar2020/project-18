a8b5a624181fc27ead5df4e8ead65c6fd6fe16fb
poduro-code <oduro.prince383@gmail.com>
2024-10-22 10:42:57 -0400

    last stage of project

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />  {/* Register route */}
-    <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
