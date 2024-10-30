5a0eb123bb91b0fdac910039e6649a24d0954b61
Abhijeet Padwal <102365270+Abhipadwal@users.noreply.github.com>
2024-10-30 23:57:29 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -9,18 +8,15 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
-
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
-
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
         default=2023,
@@ -32,6 +28,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# Ensure a newline at the end of the file
