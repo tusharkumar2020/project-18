5fac419dfec07a0a8b3ebb073c7a9aff40a4903f
Abhijeet Padwal <102365270+Abhipadwal@users.noreply.github.com>
2024-10-30 23:05:56 +0530

    Update models.py
    The changes to fix the F401, E303, E302, E251, and E501 issues reported by the linter.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,15 +1,9 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-
 # Create your models here.
-
 class CarMake(models.Model):
-    name = models.CharField(max_length =100)
+    name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
@@ -19,21 +13,22 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
+
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
-    # Other fields as needed
+        ]
+    )
 
     def __str__(self):
-        return self.name  
-
-
+        return self.name
