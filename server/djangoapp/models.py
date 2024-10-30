26b45cf712981b78825db7a4f52449ce6cf2c016
Abhijeet Padwal <102365270+Abhipadwal@users.noreply.github.com>
2024-10-31 00:01:05 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
@@ -25,6 +27,7 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ]
     )
+    
 
     def __str__(self):
         return self.name
