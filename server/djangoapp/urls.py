764c0c1c2b65b3982855fa0a50d7fdb6a5eda2c1
Abhijeet Padwal <102365270+Abhipadwal@users.noreply.github.com>
2024-10-30 23:11:44 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,29 +1,31 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
-urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name = 'registration'),
 
-    # path for login
+urlpatterns = [
+    # Path for registration
+    path(route='register', view=views.registration, name='registration'),
+
+    # Path for login
     path(route='login', view=views.login_user, name='login'),
 
-    path(route='logout', view=views.logout_request, name = 'logout'),
+    # Path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    # Path for getting cars
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
+    # Paths for dealerships
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-
+    # Path for dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
-    # path for add a review view
+    # Path for reviews and adding a review
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
-
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
