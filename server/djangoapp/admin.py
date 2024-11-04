5622f76c7af2ace36eac3f77def94c634578aae7
dlihawi <115212842+dlihawi@users.noreply.github.com>
2024-11-04 13:05:51 +0300

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import CarModel, CarMake
-
 # Register models here with their respective admins
 admin.site.register(CarModel)
 admin.site.register(CarMake)
-
