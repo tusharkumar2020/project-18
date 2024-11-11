5bd0298f06266b5e23ee8a77e118c2e12600fe58
Avaneeshakrishna <avaneesh.shastry@gmail.com>
2024-11-11 18:47:11 -0500

    Register Carmake and CarModel with admin site

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS=['localhost','https://avaneeshshas-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://avaneeshshas-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=['https://avaneeshshas-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 #CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
