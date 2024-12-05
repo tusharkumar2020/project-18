bf4af449a2e08c8974732b7436e13a50f7f18c5d
Vahidyo <156457134+Vahidyo@users.noreply.github.com>
2024-12-05 12:12:28 -0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
+#from django.core.validators import MaxValueValidator, MinValueValidator
+#from django.utils.timezone import now
 
 
 # Create your models here.
@@ -14,10 +14,21 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - __str__ method to print a car make object
 
 class CarMake(models.Model):
-    name = models.CharField(max_length=100, unique=True, help_text="The name of the car make.")
-    description = models.TextField(blank=True, help_text="A brief description of the car make.")
-    country_of_origin = models.CharField(max_length=100, blank=True, help_text="The country where the car make originates.")
-    established_year = models.PositiveIntegerField(null=True, blank=True, help_text="The year the car make was established.")
+    name = models.CharField(max_length=100,
+                            unique=True,
+                            help_text="The name of the car make."
+                           )
+    description = models.TextField(blank=True,
+                                   help_text="A brief description of the car make."
+                                  )
+    country_of_origin = models.CharField(max_length=100,
+                                         blank=True,
+                                         help_text="The country where the car make originates."
+                                        )
+    established_year = models.PositiveIntegerField(null=True,
+                                                   blank=True,
+                                                   help_text="The year the car make was established."
+                                                  )
 
     def __str__(self):
         return self.name
@@ -36,18 +47,31 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
     # Relationship to CarMake
-    car_make = models.ForeignKey('CarMake', on_delete=models.CASCADE, related_name='car_models', help_text="The car make this model belongs to.")
+    car_make = models.ForeignKey('CarMake',
+                                 on_delete=models.CASCADE,
+                                 related_name='car_models',
+                                 help_text="The car make this model belongs to."
+                                )
     
     # Fields
-    name = models.CharField(max_length=100, help_text="The name of the car model.")
+    name = models.CharField(max_length=100,
+                            help_text="The name of the car model."
+                           )
     TYPE_CHOICES = [
         ('Sedan', 'Sedan'),
         ('SUV', 'SUV'),
         ('Wagon', 'Wagon'),
     ]
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES, help_text="The type of car model.")
-    year = models.IntegerField(default=2023,help_text="The year this car model was manufactured.")
-    color = models.CharField(max_length=50, blank=True, help_text="The color of the car model.")  # Optional field
+    type = models.CharField(max_length=10,
+                            choices=TYPE_CHOICES,
+                            help_text="The type of car model."
+                           )
+    year = models.IntegerField(default=2023,
+                               help_text="The year this car model was manufactured."
+                              )
+    color = models.CharField(max_length=50,
+                             blank=True, help_text="The color of the car model."
+                            )
 
     def __str__(self):
         return f"{self.car_make.name} {self.name}"  # Combines car make and model name
