74db27092da54e0636f44a250ab14f255e47bbbd
Vahidyo <156457134+Vahidyo@users.noreply.github.com>
2024-12-05 12:37:04 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -30,9 +30,10 @@ urlpatterns = [
     ),
 
     # path for add a review view
-    path(route='reviews/dealer/<int:dealer_id>',
-         view=views.get_dealer_reviews,
-         name='dealer_details'
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_details'
     ),
     path(route='add_review', view=views.add_review, name='add_review'),
 
