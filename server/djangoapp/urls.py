b5e956fad2896627e752f3d7c86606dd18adc76d
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-12-09 07:43:56 -0500

    Uncomment path entry for 'login' in 'urls.py'

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
 
