c6c4a50f82d9b031d2d45d38feae1bc01b8b76a4
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-12-10 03:12:51 -0500

    Add 'registration' implemtation in views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -49,12 +49,39 @@ def logout_request(request):
     # Return the response
     return JsonResponse(data)
 
-# ...
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+def registration(request):
+    context = {}
+
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['first name']
+    last_name = data['lastname']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        # Check if user already exists
+        user.objects.get(username=username)
+        username_exist = True
+    except:
+        # If no, simply log this is a new user
+        logger.debug("{} is new user".format(username))
+
+    # If it is a new user:
+    if not username_exist:
+        # Create user in auth_user
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        # Login the user and redirect to list page
+        login(request, user)
+        data = {"userName":username, "status":"Authenticated"}
+        return JsonResponse(data)
+    else:
+        data = {"userName":username, "error":"Already Registered"}
+        return JsonResponse(data)
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
