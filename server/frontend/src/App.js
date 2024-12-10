1a2b4ca33cec41d2d7db1214e3ee968b882744f2
Israel-Arendse <israelarendsefortnite@gmail.com>
2024-12-10 03:58:35 -0500

    Correct <Register/> element and add 'import Register' statement

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,12 @@
 import LoginPanel from "./components/Login/Login"
+import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />,
-      <Route path="/registration" element={<RegistrationPanel />} />
+      <Route path="/registration" element={<Register/>} />,
     </Routes>
   );
 }
