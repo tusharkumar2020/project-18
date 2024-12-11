dc597da79d9bb895a9c38ca5a8980c3a7f568686
Bhavan-arya <bhavanaryatulase@gmail.com>
2024-12-11 10:45:52 -0500

    sentianalyzer test1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,4 +16,9 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),  # Ensure to include trailing slash
 
     # Additional paths can be added here
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
