b8d7982b5ffaaf2a0cddf876a069bc0c401c91db
“sinensis1511” <saramartignoni@yahoo.it>
2024-12-14 06:54:23 -0500

    Add REACT Component Dealer showing reviews

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
 
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -110,6 +110,7 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
+
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
