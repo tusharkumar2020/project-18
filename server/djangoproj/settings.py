b463c7486be8325abed9448ba266fef21ed38bc9
“sinensis1511” <saramartignoni@yahoo.it>
2024-12-14 04:32:41 -0500

    fix 403 admin page causato da / in fondo a ALLOWED_HOSTS e CSRF_TRUSTED_ORIGINS in settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS=['localhost','https://saramartigno-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://saramartigno-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS=['https://saramartigno-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
