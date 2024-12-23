f451cf42626e18ffa1d7625a8cbf8fde67f43b1e
rafaqptest <rafaqptesting@gmail.com>
2024-12-23 09:10:14 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    ('https://rafaquintero-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')
+    ('https://rafaquintero-8000.theiadockernext-1-labs-\
+    prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')
 ]
 CSRF_TRUSTED_ORIGINS = [
-    ('https://rafaquintero-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')
+    ('https://rafaquintero-8000.theiadockernext-1-labs-\
+    prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')
 ]
 
 
