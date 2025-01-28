a5476b41966c6c9211b51fa8b3399ed319627489
nilajean <nilajeanyy@gmail.com>
2025-01-28 15:04:53 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,10 +8,10 @@ app_name = 'djangoapp'
 urlpatterns = [
     # Path for registration
     path('register', views.registration, name='register'),
-    
+
     # Path for login
     path('login', views.login_user, name='login'),
-    
+
     # Path for logout
     path('logout', views.logout_request, name='logout'),
 
@@ -20,12 +20,24 @@ urlpatterns = [
 
     # Path for dealer reviews view
     path('get_dealers/', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
-    
+    path(
+        'get_dealers/<str:state>',
+        views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+
     # Path for dealer details view
-    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path(
+        'dealer/<int:dealer_id>',
+        views.get_dealer_details,
+        name='dealer_details'
+    ),
 
     # Path for add a review view
-    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_reviews'),
+    path(
+        'reviews/dealer/<int:dealer_id>',
+        views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
     path('add_review', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
