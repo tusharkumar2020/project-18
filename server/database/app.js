b54d732c66ec847e4032763391436bc22b6a4a2f
binali <binalipatel477@gmail.com>
2025-02-09 20:38:22 -0500

    View and add reviews

--- server/database/app.js
+++ server/database/app.js
@@ -49,7 +49,7 @@ app.get('/fetchReviews', async (req, res) => {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
   try {
-    const documents = await Reviews.find({dealership: req.params.id});
+    const documents = await Reviews.find({ dealership: req.params.id });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -95,17 +95,29 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   const documents = await Reviews.find().sort( { id: -1 } )
   let new_id = documents[0]['id']+1
 
-  const review = new Reviews({
-		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
-	});
+//   const review = new Reviews({
+// 		"id": new_id,
+// 		"name": data['name'],
+// 		"dealership": data['dealership'],
+// 		"review": data['review'],
+// 		"purchase": data['purchase'],
+// 		"purchase_date": data['purchase_date'],
+// 		"car_make": data['car_make'],
+// 		"car_model": data['car_model'],
+// 		"car_year": data['car_year'],
+// 	});
+
+const review = new Reviews({
+    id: new_id,
+    name: data.name,
+    dealership: data.dealership,
+    review: data.review,
+    purchase: data.purchase,
+    purchase_date: data.purchase_date,
+    car_make: data.car_make,
+    car_model: data.car_model,
+    car_year: data.car_year,
+  });
 
   try {
     const savedReview = await review.save();
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
-       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
