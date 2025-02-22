7f149396d73476a9a08653861d6e8c40da3d39ea
danhcon123 <gauvatich123@gmail.com>
2025-02-22 10:39:49 -0500

    dealer_id

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,7 +12,10 @@ def initiate_car_makes():
     # Create Car Maker Intances
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+        car_make_instances.append(CarMake.objects.create(
+            name=data['name'],
+            description=data['description']
+        ))
 
 def initiate_car_models():
     # Get the CarMake instances
@@ -24,21 +27,21 @@ def initiate_car_models():
 
     # Create CarModel instances with the corresponding Car Maker instances
     car_model_data = [
-        {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":nissan},
-        {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":nissan},
-        {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":nissan},
-        {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":mercedes},
-        {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":mercedes},
-        {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":mercedes},
-        {"name":"A4", "type":"SUV", "year": 2023, "car_make":audi},
-        {"name":"A5", "type":"SUV", "year": 2023, "car_make":audi},
-        {"name":"A6", "type":"SUV", "year": 2023, "car_make":audi},
-        {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":kia},
-        {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":kia},
-        {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":kia},
-        {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":toyota},
-        {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":toyota},
-        {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":toyota},
+        {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":nissan, "dealer_id":1},
+        {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":nissan, "dealer_id":2},
+        {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":nissan, "dealer_id":3},
+        {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":mercedes, "dealer_id":4},
+        {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":mercedes, "dealer_id":5},
+        {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":mercedes, "dealer_id":6},
+        {"name":"A4", "type":"SUV", "year": 2023, "car_make":audi, "dealer_id":7},
+        {"name":"A5", "type":"SUV", "year": 2023, "car_make":audi, "dealer_id":8},
+        {"name":"A6", "type":"SUV", "year": 2023, "car_make":audi, "dealer_id":9},
+        {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":kia, "dealer_id":10},
+        {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":kia, "dealer_id":11},
+        {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":kia, "dealer_id":12},
+        {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":toyota, "dealer_id":13},
+        {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":toyota, "dealer_id":14},
+        {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":toyota, "dealer_id":15},
     ]
 
     for data in car_model_data:
@@ -46,7 +49,8 @@ def initiate_car_models():
             name=data["name"],
             type=data["type"],
             year=data["year"],
-            car_make=data["car_make"]
+            car_make=data["car_make"],
+            dealer_id=data["dealer_id"],
         )
 
 def initiate():
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -93,6 +93,7 @@ def get_cars(request):
         initiate()
     if CarModel.objects.filter().count() == 0:
         initiate_car_models()
+        print("Car Model were created.")
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
