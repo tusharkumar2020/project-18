b47570f272c94608fa13726bf8b7bd0fd8aa56b7
danhcon123 <gauvatich123@gmail.com>
2025-02-23 04:23:11 -0500

    sentiment_analyzer

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -35,7 +35,17 @@ def get_request(endpoint, **kwargs):
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url + "analyze/" + text
+    try:
+        # Call get method of requests library with URL and params
+        response = requests.get(request_url)
+        return response.json()
+        
+    except Exception as err:
+        print(f"Unexpected {err = }, {type(err) = }")
+        print("Network exception occured.")
+        
 
 # def post_review(data_dict):
 # Add code for posting review
