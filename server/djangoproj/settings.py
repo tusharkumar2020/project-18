424eb2264547af1edd4e2cc5927196d5bc3cbed5
Santh-Sandhya85 <santhsandhya5@gmail.com>
2025-03-03 12:38:27 -0500

    build carModel and carMake models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://sandhyasanth-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://sandhyasanth-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [ 'localhost',
+       '127.0.0.1',
+       'https://sandhyasanth-8002.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',]
+CSRF_TRUSTED_ORIGINS = ['https://sandhyasanth-8002.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+       'http://127.0.0.1:8002',]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
