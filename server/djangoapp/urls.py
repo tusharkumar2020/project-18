d35f5796320e7c3bc14a4eea8239bbb26582346f
Vegablues <eminyeni@gmail.com>
2025-04-08 04:30:37 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
         path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
         path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
         path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-        
+    path('add_review', views.add_review, name='add_review'),
+             
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -10,9 +10,10 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterContainer />} />
-      <Route path="/dealers" element={<Dealers/>} />
-        <Route path="/dealer/:id" element={<Dealer/>} />
-        <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+        
     </Routes>
   );
 }
