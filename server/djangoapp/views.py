c02e844ad50c1edf9673954d2a92e28a5df0b0b6
TeyahC <teyahc@hotmail.com>
2025-04-11 04:58:42 -0400

    Dealerships Page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -90,7 +90,6 @@ def get_cars(request):
     return JsonResponse({"CarModels":cars})
 
 
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
