eb2022916483190ec04484cca182396333cb98a8
TeyahC <teyahc@hotmail.com>
2025-04-22 08:44:38 -0400

    Module 4: Dealer details now woek but not the reviews

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -101,7 +101,7 @@ def get_dealerships(request, state="All"):
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
-        endpoint = "fetchReviews/dealer/"+str(dealer_id)
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
