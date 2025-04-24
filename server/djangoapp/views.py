d6c5e12b50e29da89e131bdcedc615cdbe48c12d
TeyahC <teyahc@hotmail.com>
2025-04-24 05:51:01 -0400

    Module 4: Reviews work but cant post them

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -131,12 +131,16 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status":200})
+            return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
+        finally:
+            print("add_review request successful!")
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
