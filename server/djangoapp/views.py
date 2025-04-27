979e5c45608b3894dc94be474290e62da75bcf8e
Vamsi23 <kalapalavamsi23@gmail.com>
2025-04-27 23:11:41 +0530

    ci&&cd3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,7 +14,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, # post_review
+from .restapis import get_request, analyze_review_sentiments # post_review
 from .populate import initiate
 
 
