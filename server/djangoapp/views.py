01910bc090ab36da1ce36cbbbe30f175fc2eebf8
TeyahC <teyahc@hotmail.com>
2025-04-29 05:40:59 -0400

    Formatting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -24,13 +24,14 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
     # Get username and password from request.POST dictionary
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
+    username = data["userName"]
+    password = data["password"]
     # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
@@ -40,10 +41,11 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
 
 
@@ -52,11 +54,11 @@ def logout_request(request):
 def registration(request):
     context = {}
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
+    username = data["userName"]
+    password = data["password"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    email = data["email"]
     username_exist = False
     email_exist = False
     try:
@@ -69,49 +71,59 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email,
+        )
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if count == 0:
         initiate()
-    car_models = CarModel.objects.select_related('car_make')
+    car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
+    return JsonResponse({"CarModels": cars})
 
 
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if state == "All":
         endpoint = "fetchDealers"
     else:
-        endpoint = "fetchDealers/"+state
+        endpoint = "fetchDealers/" + state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealers":dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
+
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if(dealer_id):
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
+            response = analyze_review_sentiments(review_detail["review"])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status":200,"reviews":reviews})
+            review_detail["sentiment"] = response["sentiment"]
+        return JsonResponse({"status": 200, "reviews": reviews})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 import json
+
 
 def get_dealer_details(request, dealer_id):
     if dealer_id:
@@ -129,18 +141,15 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"message": "Bad Request"}, status=400)
 
 
-
 def add_review(request):
-    if (request.user.is_anonymous is False):
+    if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status": 401,
-                                 "message": "Error in posting review"})
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
         finally:
             print("add_review request successful!")
     else:
-        return JsonResponse({"status": 403,
-                             "message": "Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -20,6 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+<<<<<<< HEAD
     path('admin/', admin.site.urls), 
     path('djangoapp/', include('djangoapp.urls')), 
     path('', TemplateView.as_view(template_name="Home.html")), 
@@ -30,4 +32,18 @@ urlpatterns = [
     path('dealers/', TemplateView.as_view(template_name="index.html")), 
     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")), 
         path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")), 
+=======
+    path("admin/", admin.site.urls),
+    path("djangoapp/", include("djangoapp.urls")),
+    path("", TemplateView.as_view(template_name="Home.html")),
+    path("about/", TemplateView.as_view(template_name="About.html")),
+    path("contact/", TemplateView.as_view(template_name="Contact.html")),
+    path("login/", TemplateView.as_view(template_name="index.html")),
+    path("register/", TemplateView.as_view(template_name="index.html")),
+    path("dealers/", TemplateView.as_view(template_name="index.html")),
+    path("dealer/<int:dealer_id>", TemplateView.as_view(template_name="index.html")),
+    path(
+        "postreview/<int:dealer_id>", TemplateView.as_view(template_name="index.html")
+    ),
+>>>>>>> 2eef632 (Formatting)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
