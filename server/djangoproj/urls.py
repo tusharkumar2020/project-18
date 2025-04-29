8ea8d3ef66eb24e85fed4078a282c44e984567cb
TeyahC <teyahc@hotmail.com>
2025-04-29 05:56:05 -0400

    Formatting

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,18 +21,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('admin/', admin.site.urls), 
-    path('djangoapp/', include('djangoapp.urls')), 
-    path('', TemplateView.as_view(template_name="Home.html")), 
-    path('about/', TemplateView.as_view(template_name="About.html")), 
-    path('contact/', TemplateView.as_view(template_name="Contact.html")), 
-    path('login/', TemplateView.as_view(template_name="index.html")), 
-    path('register/', TemplateView.as_view(template_name="index.html")), 
-    path('dealers/', TemplateView.as_view(template_name="index.html")), 
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")), 
-        path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")), 
-=======
     path("admin/", admin.site.urls),
     path("djangoapp/", include("djangoapp.urls")),
     path("", TemplateView.as_view(template_name="Home.html")),
@@ -45,5 +33,4 @@ urlpatterns = [
     path(
         "postreview/<int:dealer_id>", TemplateView.as_view(template_name="index.html")
     ),
->>>>>>> 2eef632 (Formatting)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
