0859cd404ddcd31d7bed28c171e8c7fdc2ced25b
Maryam Kanso <maryam@happywp.co>
2025-04-29 16:52:07 -0400

    Describe what you changed

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,9 +6,10 @@ from django.conf import settings
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
 
     # path for login
+
     # path(route='login', view=views.login_user, name='login'),
 
     # path for dealer reviews view
