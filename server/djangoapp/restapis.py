ec25e3c655c8f28c5d078f6ac7dce5681013630a
Silvafox76 <ryan.w.dear@gmail.com>
2025-05-01 12:53:55 -0400

    ReactPages and CarTables

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,13 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
-# def get_request(endpoint, **kwargs):
+
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
@@ -31,8 +30,9 @@ def get_request(endpoint, **kwargs):
     except Exception as e:
         print(f"Error: {e}")
 
+
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -40,6 +40,7 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
 
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# Uncomment the required imports before adding the code
+
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 from .models import CarMake, CarModel
 
 from django.http import JsonResponse
@@ -15,11 +12,13 @@ from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .restapis import get_request, analyze_review_sentiments, post_review
 
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -44,37 +43,40 @@ def logout_request(request):
     data = {"userName": ""}
     return JsonResponse(data)
 
+
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
-
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
+    username = data["userName"]
+    password = data["password"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    email = data["email"]
     username_exist = False
-    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
-
+    except Exception as e:
+        # If an exception occurs, log it with a message about the new user
+        logger.debug(f"Error occurred: {e}. {username} is a new user.")
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email,
+        )
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
@@ -91,6 +93,7 @@ def get_cars(request):
             "CarMake": car_model.car_make.name,
         })
     return JsonResponse({"CarModels": cars})
+
 
 def get_dealerships(request, state="All"):
     if state == "All":
