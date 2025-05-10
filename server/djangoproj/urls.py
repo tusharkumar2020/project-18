1da2e8ad7ed30f3f65e8099da0f959053bd1622f
Soma_sri_Pavan_kumar <pavank15140@gmail.com>
2025-05-10 22:04:44 +0530

    commit

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,6 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', TemplateView.as_view(template_name="about.html")),
+    path('contact/', TemplateView.as_view(template_name="contact.html")),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
