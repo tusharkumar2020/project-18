fb298ab0a1cd73d643eaf2264a0b6d042b747e9a
madhubala <madhubalaselvaraj94@gmail.com>
2025-05-14 10:09:34 -0400

    updated app.js

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,39 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealers = await Dealerships.find();
+        res.json(dealers);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers' });
+      }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const state = req.params.state;
+        const dealers = await Dealerships.find({ state: state });
+        res.json(dealers);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealers by state' });
+      }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const dealerId = parseInt(req.params.id);
+        const dealer = await Dealerships.findOne({ id: dealerId });
+    
+        if (!dealer) {
+          return res.status(404).json({ error: 'Dealer not found' });
+        }
+    
+        res.json(dealer);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealer by ID' });
+      }
 });
 
 //Express route to insert review
