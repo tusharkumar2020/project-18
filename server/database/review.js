9ea4137df238455c2d04f1759c6fbfe05754ff6c
madhubala <madhubalaselvaraj94@gmail.com>
2025-05-15 12:44:16 -0400

    updated review file

--- server/database/review.js
+++ server/database/review.js
@@ -1,47 +1,49 @@
-
-'use strict';
-
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-
-const reviewSchema = new Schema({
-  id: {
-    type: Number,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  dealership: {
-    type: Number,
-    required: true
-  },
-  review: {
-    type: String,
-    required: true
-  },
-  purchase: {
-    type: Boolean,
-    required: true
-  },
-  purchase_date: {
-    type: String,
-    required: true
-  },
-  car_make: {
-    type: String,
-    required: true
-  },
-  car_model: {
-    type: String,
-    required: true
-  },
-  car_year: {
-    type: Number,
-    required: true
-  }
-});
-
-module.exports = mongoose.model('Review', reviewSchema);
+/* jshint node: true, esversion: 6 */
+(function () {
+    'use strict';
+  
+    const mongoose = require('mongoose');
+    const { Schema } = mongoose;
+  
+    const reviewSchema = new Schema({
+      id: {
+        type: Number,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      dealership: {
+        type: Number,
+        required: true
+      },
+      review: {
+        type: String,
+        required: true
+      },
+      purchase: {
+        type: Boolean,
+        required: true
+      },
+      purchase_date: {
+        type: String,
+        required: true
+      },
+      car_make: {
+        type: String,
+        required: true
+      },
+      car_model: {
+        type: String,
+        required: true
+      },
+      car_year: {
+        type: Number,
+        required: true
+      }
+    });
+  
+    module.exports = mongoose.model('Review', reviewSchema);
+  })();
+  
