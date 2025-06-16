b72af92486e8043c4cb0a6a762487bf19cf43030
ngahoang <nhoang2@seattleu.edu>
2025-06-16 15:15:12 -0400

    Implemented Django proxy services to integrate dealer and review data from external Express APIs

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,37 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params =""
+    if (kwargs):
+        for key,value in kwargs.items():
+            params = params+key+"="+value+"&"
+    
+    request_url = backend_url+endpoint+"?"+params
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+    print("get from {}".format(request_url))
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        print('network exception occured')
 
-# def post_review(data_dict):
-# Add code for posting review
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"unexpected{err=},{type(err)=}")
+        print('network exception occurred')
+
+
+
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.get(request_url)
+        print(response.json())
+        return response.json()
+    except:
+        print('network exception occurred')
