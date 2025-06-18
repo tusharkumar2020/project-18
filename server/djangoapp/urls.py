1691a00775fc12678e700366712635062a816ab3
fardowza <60208943+fardowza@users.noreply.github.com>
2025-06-18 10:37:37 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     # # path for registration
     path('register', view=views.registration, name='register'),
 
@@ -19,9 +19,12 @@ urlpatterns = [
 
     # path for dealer reviews view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>',
+         view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
