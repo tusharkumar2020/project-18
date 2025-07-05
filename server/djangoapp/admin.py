fa9cf53e72a020677fba53ef100de06b59849f6c
Aashiq-N <aashiq.mymail@gmail.com>
2025-07-05 08:33:17 -0400

    Module 3-2 Complete

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,11 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
 
-# Register your models here.
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
